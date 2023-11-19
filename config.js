b4041981cb168d49f4bec8f6b4025f9e5f3ea973
whathappbot <150361683+whathappbot@users.noreply.github.com>
2023-11-19 17:10:32 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['94767939688', 'gaya', true],
+  ['94767939688', 'gaya', true], 
   [''] 
 ] //Number of owners
 
